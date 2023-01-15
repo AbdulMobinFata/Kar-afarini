08bc3d30253fc806aa5e253343ade457c9d9ce95
Abdul Mobin Fata <Fatapjr2020@gmail.com>
2023-01-15 15:04:51 +0330

    F.1

---
+++ script.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector('.slider');
+
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+
+const indicatorParents = document.querySelector('.controls ul');
+
+var sectionIndex = 0;
+
+document.querySelectorAll('.controls li').forEach(function (indicator, ind) {
+    indicator.addEventListener('click', function () {
+        sectionIndex = ind;
+        document.querySelector('.controls .selected').classList.remove('selected');
+        slider.style.transform = 'translate(' + (sectionIndex) * 33 + '%)'; 
+        indicator.classList.add('selected');
+    });
+});
+
+rightArrow.addEventListener('click', function () {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate('+(sectionIndex) * 33 + '%)';
+});
+
+leftArrow.addEventListener('click', function () {
+    sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 2;
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate('+(sectionIndex) * 33 + '%)';
+});
